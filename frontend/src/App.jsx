94be1dc60407f8200f41f0d9360580988d9ebd3c
caokhanhquynh <kqc@sfu.ca>
2025-06-23 20:35:00 -0700

    set up MUI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
 
 function App() {
   const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
@@ -13,6 +17,11 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Button variant="contained">Hello world</Button>
+      <Badge badgeContent={4} color="primary">
+        <MailIcon color="action" />
+      </Badge>
+
       <nav style={{ padding: '1rem' }}>
         <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
         <Link to="/about">About</Link>
