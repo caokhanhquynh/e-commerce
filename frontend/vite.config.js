21df61e084da4fc4100b6651d51b84213dfa019e
caokhanhquynh <kqc@sfu.ca>
2025-06-23 23:06:16 -0700

    test login api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-  server: {
-    port: 5173,
-    proxy: {
-      '/weatherforecast': {
-        target: 'https://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
