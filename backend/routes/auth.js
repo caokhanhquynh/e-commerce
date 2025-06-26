32117e27c5b82e3a281d07bed3914dda946e701c
caokhanhquynh <kqc@sfu.ca>
2025-06-26 12:02:56 -0700

    update user api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -24,7 +24,7 @@ router.post('/register', [
     try {
         const hash = await bcrypt.hash(password, 10);
         const result = await pool.query(
-        'INSERT INTO sellers (name, email, password) VALUES ($1, $2, $3) RETURNING sid, email',
+        'INSERT INTO users (name, email, password, type) VALUES ($1, $2, $3, \'user\') RETURNING id, email',
         [name, email, hash]
         );
         res.status(201).json(result.rows[0]);
@@ -39,15 +39,15 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const result = await pool.query('SELECT * FROM sellers WHERE email = $1', [email]);
+        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
         if (result.rows.length === 0) return res.status(400).json({ msg: 'User not found' });
 
-        const sellers = result.rows[0];
-        const match = await bcrypt.compare(password, sellers.password);
+        const user = result.rows[0];
+        const match = await bcrypt.compare(password, user.password);
         // const match = (password == sellers.password)
         if (!match) return res.status(401).json({ msg: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: sellers.id, email: sellers.email }, JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
         res.json({ token });
     } catch (err) {
         console.error(err);
