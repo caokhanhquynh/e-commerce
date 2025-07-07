977d2e28cddef1a08135867137c28ea4c81660a5
caokhanhquynh <kqc@sfu.ca>
2025-07-06 23:42:54 -0700

    add wishlists api

--- backend/routes/wishlists.js
+++ backend/routes/wishlists.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 
 router.get('/', async (req, res) => {
     try {
-      const result = await pool.query('SELECT * FROM carts');
+      const result = await pool.query('SELECT * FROM wishlists');
       res.json(result.rows);
     } catch (err) {
       console.error('Error querying database:', err);
@@ -19,14 +19,14 @@ router.get('/', async (req, res) => {
     }
 });
 
-// GET /api/carts/:id
+// GET /api/wishlists/:id
 router.get('/:id', async (req, res) => {
     const userId = req.params.id;
   
     try {
       const result = await pool.query(`
-        SELECT c.*, i.title, i.photo, i.price
-        FROM carts c
+        SELECT *
+        FROM wishlists c
         JOIN items i ON c.iid = i.iid
         WHERE c.id = $1
       `, [userId]);
@@ -42,20 +42,8 @@ router.post('/', async (req, res) => {
     const { id, iid } = req.body;
 
     try {
-        const existing = await pool.query(
-            'SELECT quantity FROM carts WHERE id = $1 AND iid = $2',
-            [id, iid]
-        );
-        if (existing.rowCount > 0) {
-            const updated = await pool.query(
-              'UPDATE carts SET quantity = quantity + 1 WHERE id = $1 AND iid = $2 RETURNING id, iid, quantity',
-              [id, iid]
-            );
-            return res.status(200).json({ message: 'Item quantity updated', data: updated.rows[0] });
-        }
-
         const result = await pool.query(
-            'INSERT INTO carts (id, iid, quantity) VALUES ($1, $2, 1) RETURNING id, iid',
+            'INSERT INTO wishlists (id, iid) VALUES ($1, $2) RETURNING id, iid',
             [id, iid]
             );
         res.status(201).json({ message: 'Item created', data: result.rows[0] });
